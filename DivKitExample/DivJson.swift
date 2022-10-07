7ae277f527e45c67b2d0c044abe200eb5c192d42
Aleksandr Bolotov <Alexander.Bolotov@onlyoffice.com>
2022-10-07 09:50:36 +0300

    DivKitExample

---
+++ DivKitExample/DivJson.swift
@@ -0,0 +1,32 @@
+//
+//  DivJson.swift
+//  DivKitExample
+//
+//  Created by Aleksandr Bolotov on 04.10.2022.
+//
+
+import DivKit
+import Serialization
+import TemplatesSupport
+
+public struct DivJson: Deserializable {
+  public let templates: [String: Any]
+  public let cards: [[String: Any]]
+
+  public init(dictionary: [String: Any]) throws {
+    templates = try dictionary.getOptionalField("templates") ?? [:]
+    cards = try dictionary.getArray("cards")
+  }
+
+  static func loadCards() throws -> [DivData] {
+    let url = Bundle.main.url(forResource: "div_json", withExtension: "json")!
+    let data = try Data(contentsOf: url)
+    let divJson = try DivJson(JSONData: data)
+    return divJson.cards.compactMap {
+      DivData.resolve(
+        card: $0,
+        templates: divJson.templates
+      ).value
+    }
+  }
+}
--- DivKitExample/ViewController.swift
+++ DivKitExample/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import DivKit
+import LayoutKit
+import Serialization
 
 class ViewController: UIViewController {
+    private var divHostView: DivHostView!
+    private var components: DivKitComponents!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        components = DivKitComponents(
+            updateCardAction: nil,
+            urlOpener: { UIApplication.shared.open($0) }
+        )
+        divHostView = DivHostView(components: components)
+
+        if let cards = try? DivJson.loadCards() {
+            view.addSubview(divHostView)
+            divHostView.setData(cards)
+        }
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        divHostView.frame = view.bounds.inset(by: view.safeAreaInsets)
+    }
+}
 
+extension ViewController: UIActionEventPerforming {
+    func perform(uiActionEvent event: UIActionEvent, from sender: AnyObject) {
+        switch event.payload {
+        case let .divAction(params):
+            components.handleActions(params: params)
+            divHostView.reloadItem(cardId: params.cardId)
+        case .empty,
+                .url,
+                .menu,
+                .json,
+                .composite:
+            break
+        }
+    }
 }
 
